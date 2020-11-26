f0cb1e1c7c236568c40765e12fa2ba439667b151
879yu <71927591+879yu@users.noreply.github.com>
2020-11-26 14:34:16 +0900

    Userモデルの作成

--- src/model/LoginLogic.java
+++ src/model/LoginLogic.java
@@ -1,10 +1,12 @@
 package model;
 
 public class LoginLogic {
-	public boolean execute(Login login) {
+	public boolean execute(Login login, User user) {
+		int userId = login.getUserId();
+		String password = login.getPassword();
 
 		// ユーザーID：0000 パスワード：passでログインする（仮）
-		if((login.getUserId() == 0000) && (login.getPassword().equals("pass"))) {
+		if((login.getUserId() == userId) && (login.getPassword().equals(password))) {
 			return true;
 		}
 
--- src/model/UserDAO.java
+++ src/model/UserDAO.java
@@ -4,36 +4,48 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class UserDAO extends ConnectionDAO{
 	// 実行するSQL文を文字列として事前に設定
-		final String SQL = "select * from users where user_id = ? in (select * password = ? from users)";
+		final String SQL = "SELECT * FROM USERS WHERE USER_ID = ? HAVING PASSWORD = ?;";
 
-	public List<Login> findByLogin(Login login) {
+	public User findByLogin(Login login) {
 
-		// ログイン情報（ID、パスワード）を変数に格納
+		// ユーザーが入力したログイン情報（ID、パスワード）を変数に格納
 		int userId = login.getUserId();
 		String password = login.getPassword();
 
-		// ログインしたユーザーの情報を格納するリスト（戻り値の準備）
-		List<Login> list = new ArrayList<>();
+		// 戻り値をセットするインスタンスを生成
+		User user = new User();
 
 		try(Connection conn = getConn();
 				PreparedStatement pstm = conn.prepareStatement(SQL)){
 
 
+			// プレースホルダにユーザーが入力した情報をセット
 			pstm.setInt(1,userId);
 			pstm.setString(2,password);
 
 			ResultSet rs = pstm.executeQuery();
 
+			if(rs.next()) {
+				// 見つかったユーザー情報をインスタンスにセットする（戻り値の準備）
+
+				user.setUserId(rs.getInt(1));
+				user.setUserName(rs.getString(2));
+				user.setPassword(rs.getString(3));
+
+			}else {
+				// 無い場合はnullを返す
+				return null;
+			}
+
 		}catch (SQLException e) {
 			System.out.println("DB処理で問題が発生しました");
+			return null;
 		}
 
-		return list;
+		return user;
 	}
 }
 
