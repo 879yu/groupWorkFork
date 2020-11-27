c453ebaa65536a8263ee6e116b72bb9f438c3278
879yu <71927591+879yu@users.noreply.github.com>
2020-11-27 10:39:35 +0900

    LoginLogicとLoginServletの人力マージ

---
+++ src/model/LoginLogic.java
@@ -0,0 +1,45 @@
+package model;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginLogic {
+	public void execute(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+		// idとpasswordを取得
+		request.setCharacterEncoding("utf-8");
+		int userId=Integer.parseInt(request.getParameter("user_id"));
+		String password=request.getParameter("password");
+
+		// 取得したログイン情報をLoginLogicに渡す
+		Login login=new Login(userId,password);
+
+		// Usersテーブルの検索
+		UserDAO userDAO=new UserDAO();
+		User user=new User();
+		user=userDAO.findByLogin(login);
+
+		String forwardPass;
+		if(user.getUserId()==0) {
+			forwardPass= "/WEB-INF/jsp/admin.jsp";
+		}else if(user==null) {
+			forwardPass= "/WEB-INF/jsp/fail.jsp";
+		}else {
+			forwardPass= "/WEB-INF/jsp/mypage.jsp";
+		}
+
+		// セッションスコープの利用開始
+		HttpSession session = request.getSession();
+
+		// 取得したArrayListをセッションスコープに保存
+		session.setAttribute("user", user);
+
+		RequestDispatcher dsp=request.getRequestDispatcher(forwardPass);
+		dsp.forward(request,response);
+	}
+
+}
--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -9,8 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import model.Login;
 import model.LoginLogic;
+
 
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
@@ -28,42 +28,9 @@ public class LoginServlet extends HttpServlet {
 	 * フォームのログインボタンで成功画面にフォワード/失敗でリダイレクト
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");
-		// idとpasswordを取得
-		int userId=Integer.parseInt(request.getParameter("user_id"));
-		String password=request.getParameter("password");
 
-		// Loginモデルのインスタンスを生成
-		Login login=new Login(userId, password);
-
-		// LoginLogicのメソッドを実行
 		LoginLogic loginLogic=new LoginLogic();
-		boolean isLogin =loginLogic.execute(login);
-
-		// if文でtrue/false分岐
-		String forwardPass;
-		if(isLogin) {
-//			// AccountDAOのメソッドでuserテーブルを検索
-//			AccountDAO 変数名2=new AccountDAO();
-
-
-			// 正しいデータがあればAccountモデルのインスタンスを生成
-//			User user=new User(login);
-
-			// accountをセッションスコープに保存
-
-			// 管理者かどうかの判別
-			// 管理者ページに遷移
-			forwardPass="/WEB-INF/jsp/admin.jsp";
-
-			// 従業員用マイページに遷移
-//			forwardPass="/WEB-INF/jsp/mypage.jsp";
-
-		}else {
-			forwardPass="/WEB-INF/jsp/fail.jsp";
-		}
-		RequestDispatcher dsp=request.getRequestDispatcher(forwardPass);
-		dsp.forward(request,response);
+		loginLogic.execute(request,response);
 
 	}
 
