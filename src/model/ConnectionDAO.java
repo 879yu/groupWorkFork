ca040dbfbeb53981498e1a1ddf94cd701bcc7aaa
879yu <71927591+879yu@users.noreply.github.com>
2020-12-28 16:38:19 +0900

    データベース接続確認

--- src/model/ConnectionDAO.java
+++ src/model/ConnectionDAO.java
@@ -9,9 +9,12 @@ public class ConnectionDAO {
 	// DB接続情報の設定
 	final String URL  = "jdbc:mysql://localhost:3306/group_work?serverTimezone=JST";
 //	final String URL  = "jdbc:mysql://172.16.71.101:3306/group_work?serverTimezone=JST";
-	final String USER = "webuser02";
+	final String USER = "root";
+//	final String USER = "webuser02";
 //	final String PASS = "1234";
-	final String PASS = "1234groupwork";
+	final String PASS = "root11pass";
+//	final String PASS = "1234groupwork";
+
 
 	public Connection getConnection() throws SQLException{
 		return DriverManager.getConnection(URL, USER, PASS);
